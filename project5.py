6a66f2204a1ffb000df7d7e2b620ef1a981763b4
peilinw2 <77708662+peilinw2@users.noreply.github.com>
2021-11-28 16:05:39 +0800

    Update project5.py

--- project5.py
+++ project5.py
@@ -3,8 +3,8 @@ Math 560
 Project 5
 Fall 2021
 
-Partner 1:
-Partner 2:
+Partner 1: Amy Wang (pw137)
+Partner 2: Lu Liu (ll139)
 Date:
 """
 
@@ -20,10 +20,41 @@ from p5priorityQueue import *
 
 """
 Prim's Algorithm
+
+inputs: 
+  adjList: list of vertices
+  adjMat: matrix that contains distances between vertices
+
+outputs: none
 """
 def prim(adjList, adjMat):
-    ##### Your implementation goes here. #####
+
+# initialize the cost to infinite and previous vertex to none.
+    for vertex in adjList:
+        vertex.prev = None
+        vertex.cost = math.inf
+        vertex.visited = False
+
+# arbitrarily pick the start vertex and set the cost to 0
+    start = adjList[0]
+    start.cost = 0
+
+#construct priority queue
+    Q = PriorityQueue(adjList)
+
+    while not Q.isEmpty():
+        #visit the next unvisited vertex
+        vertex = Q.deleteMin()
+        vertex.visited = True
+# for each edge in vertex.neigh
+        for neighbor in vertex.neigh:
+#if the edge leads out, update
+            if not neighbor.visited:
+                if neighbor.cost > adjMat[vertex.rank][neighbor.rank]:
+                    neighbor.cost = adjMat[vertex.rank][neighbor.rank]
+                    neighbor.prev = vertex
     return
+
 
 ################################################################################
 
@@ -50,19 +81,32 @@ These functions will operate directly on the input vertex objects.
 
 """
 makeset: this function will create a singleton set with root v.
+
+input: vertex
+output: none
 """
+
 def makeset(v):
-    ##### Your implementation goes here. #####
+
+    v.pi = v
+    v.height = 0
+
     return
 
 """
 find: this function will return the root of the set that contains v.
 Note: we will use path compression here.
 
+input: vertex
+output: vertex's root vertex
 """
 def find(v):
-    ##### Your implementation goes here. #####
+
+    while v != v.pi:
+        v = v.pi
+
     return v.pi
+
 
 """
 union: this function will union the sets of vertices v and u.
